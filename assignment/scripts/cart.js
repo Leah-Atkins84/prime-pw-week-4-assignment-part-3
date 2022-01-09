7f7b6d684b5f6fcf7252937896b4a7cd4af80ce8
Leah Atkins <leah.atkins84@gmail.com>
2022-01-09 14:35:15 -0600

    start of assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,50 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+console.log('Empty basket array:', basket);
+
+let item = 'banana';
+{console.log(item)};
+
+
+
+
+function addItem(basket){
+  console.log();
+  basket.push(item);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//stretch goals
+const maxItems = 5;
+console.log('maxItems:', maxItems);
