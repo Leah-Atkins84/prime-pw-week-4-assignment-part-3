7f1aded07104524cf583d8db90ca29665f545507
Leah Atkins <leah.atkins84@gmail.com>
2022-01-09 18:24:55 -0600

    more assignment parts completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,21 +2,39 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = [];
-console.log('Empty basket array:', basket);
-
-let item = 'banana';
-{console.log(item)};
+let basket= [];
+console.log('empty basket array:', basket);
 
 
-
-
-function addItem(basket){
-  console.log();
-  basket.push(item);
-
+let items= (['banana', 'apples', 'oranges']);
+function addItem(items){
+  console.log('in addItem', items);
+  basket.push(items);
+  return true;
 
 }
+
+
+console.log('basket has items', addItem(items));
+console.log('Adding cucumber(expect true)', addItem('cucumber'));
+basket.push('broccoli');
+console.log('basket has these items:', basket);
+
+
+let listItems= basket
+
+listItems.forEach(element => console.log(element));
+
+function empty(basket){
+    empty = [];
+    return true;
+}
+empty();
+
+console.log('empty basket:', empty(basket));
+console.log(basket);
+
+
 
 
 
